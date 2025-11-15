7d9c20ed47baa045a94898f67736c8dd201083b2
endorsain <blanco.genaro.mauro@gmail.com>
2025-11-15 03:46:39 -0300

    test: Eliminar PATHS.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { AuthProvider } from "./provider";
+// import { AuthProvider } from "./provider";
 import { MainLayout } from "./layout";
 import { HomePage } from "./home";
 import { SettingPage } from "./setting";
-import { PATHS } from "./shared";
+// import { PATHS } from "./shared";
 import { AccessPage } from "./access";
 
 function App() {
@@ -19,11 +19,14 @@ function App() {
           } */
           element={<MainLayout />}
         >
-          <Route element={<HomePage />} path={PATHS.main.home} />
-          <Route element={<SettingPage />} path={PATHS.main.setting} />
+          {/* <Route element={<HomePage />} path={PATHS.main.home} /> */}
+          {/* <Route element={<SettingPage />} path={PATHS.main.setting} /> */}
+          <Route element={<HomePage />} path="/home" />
+          <Route element={<SettingPage />} path="/setting" />
         </Route>
         {/* Access */}
-        <Route element={<AccessPage />} path={PATHS.access} />
+        {/* <Route element={<AccessPage />} path={PATHS.access} /> */}
+        <Route element={<AccessPage />} path="/" />
       </Routes>
     </Router>
   );
