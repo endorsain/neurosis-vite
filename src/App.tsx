335cc3fedd662cef260f0d9f65e497beb7688354
endorsain <blanco.genaro.mauro@gmail.com>
2025-11-15 04:02:46 -0300

    build: vercel.json para la compilacion en vercel.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { MainLayout } from "./layout";
 import { HomePage } from "./home";
 import { SettingPage } from "./setting";
-// import { PATHS } from "./shared";
+import { PATHS } from "./shared";
 import { AccessPage } from "./access";
 
 function App() {
@@ -19,14 +19,11 @@ function App() {
           } */
           element={<MainLayout />}
         >
-          {/* <Route element={<HomePage />} path={PATHS.main.home} /> */}
-          {/* <Route element={<SettingPage />} path={PATHS.main.setting} /> */}
-          <Route element={<HomePage />} path="/home" />
-          <Route element={<SettingPage />} path="/setting" />
+          <Route element={<HomePage />} path={PATHS.main.home} />
+          <Route element={<SettingPage />} path={PATHS.main.setting} />
         </Route>
         {/* Access */}
-        {/* <Route element={<AccessPage />} path={PATHS.access} /> */}
-        <Route element={<AccessPage />} path="/" />
+        <Route element={<AccessPage />} path={PATHS.access} />
       </Routes>
     </Router>
   );
